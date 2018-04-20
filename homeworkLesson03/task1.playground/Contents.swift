f77201202d62b480653bd8d29cdc1e285793c8e9
Maurikzio <m.morocho1196@gmail.com>
2018-04-20 08:06:50 +0300

    task 1 'Create a hello world program' has been added

---
+++ homeworkLesson03/task1.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello world!"
+print(str)
+str = "Hola Mundo!"
+print(str)
+str = "Salut Monde!"
+print(str)
+
+//addition of 2 numbers
+let num1 = 10
+let num2 = 5
+print(num1 + num2)
+
+
+
