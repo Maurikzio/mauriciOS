6386b1602ab5bcb772c4137d1291cbdef133523e
Maurikzio <m.morocho1196@gmail.com>
2018-04-20 08:14:08 +0300

    task 2 'Working with types' has been added

---
+++ homeworkLesson03/task2.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+//TASK NUMBER 2
+
+import UIKit
+
+//1
+//integer number 12 (unsigned) in minimal binary bits (8) = 1100 (0b0000_1100)
+let unsigned12:UInt8 = 0b0001100
+
+//2
+//integer -100 (signed) in minimal binary bits (8) = 10011100 (0b1001_1100)
+let signedMinus100 = Int8(bitPattern: 0b10011100)
+
+//3
+//hex eqivalent for 128 = 80 (0x80)
+let hexadecimalInteger = 0x80
+
+//4
+//minimum number for 16 bit-form, signed = -32768, unsigned = 0
+let signed16Bit = Int16.min
+let unsigned16Bit = UInt16.min
+
+
+//5
+//maximum number for 64 bit-form, signed = 9223372036854775807, unsigned = 18446744073709551615
+let signed64Bit = Int64.max
+let unsigned64Bit = UInt64.max
+
+//7
+// symbol a
+let myCharacter:Character = "a"
+
+//8, line 'Hello world'
+let myString:String = "Hello world"
+
+//9
+//boolean
+let yes:Bool = true
+
+//10
+//number 12 and its string representation "twelve"
+let number12 = (12, "twelve")
+
+
+
+
+
+
+
+
+
+
